e31e2a2ac4998630c1fd9e5a1432159be9a21588
theadedmonkey <theadedmonkey@gmail.com>
2016-08-01 01:43:50 +0200

    added clear rows functionality

--- tetris.cpp
+++ tetris.cpp
@@ -37,7 +37,7 @@ std::vector<std::string> tetrominoNames = { "I", "O", "T", "S", "Z", "J", "L" };
 struct Tetromino {
   Tetromino() {}
 
-  Tetromino(std::array<std::array<int, 4>, 2> t_shape) : shape(t_shape) {}
+  Tetromino(std::array<std::array<int, 4>, 4> t_shape) : shape(t_shape) {}
 
   int col = 4;
   int row = 0;
@@ -50,7 +50,7 @@ struct Tetromino {
 
   bool hasLanded = false;
 
-  std::array<std::array<int, 4>, 2> shape;
+  std::array<std::array<int, 4>, 4> shape;
 
   bool isMoving = false;
 };
@@ -61,48 +61,62 @@ Tetromino createTetromino(std::string name) {
     return Tetromino({
       0, 0, 0, 0,
       1, 1, 1, 1,
+      0, 0, 0, 0,
+      0, 0, 0, 0
     });
   }
 
   if (name == "O") {
     return Tetromino({
-      0, 1, 1, 0,
-      0, 1, 1, 0
+      0, 2, 2, 0,
+      0, 2, 2, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
     });
   }
 
   if (name == "T") {
     return Tetromino({
-      0, 1, 0, 0,
-      1, 1, 1, 0
+      0, 3, 0, 0,
+      3, 3, 3, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
     });
   }
 
   if (name == "S") {
     return Tetromino({
-      0, 1, 1, 0,
-      1, 1, 0, 0
+      0, 4, 4, 0,
+      4, 4, 0, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
     });
   }
 
   if (name == "Z") {
     return Tetromino({
-      1, 1, 0, 0,
-      0, 1, 1, 0
+      5, 5, 0, 0,
+      0, 5, 5, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
     });
   }
 
   if (name == "J") {
     return Tetromino({
-      1, 0, 0, 0,
-      1, 1, 1, 0
+      6, 0, 0, 0,
+      6, 6, 6, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
     });
   }
 
   if (name == "L") {
     return Tetromino({
-      0, 0, 1, 0,
-      1, 1, 1, 0
+      0, 0, 7, 0,
+      7, 7, 7, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
     });
   }
 }
@@ -110,8 +124,15 @@ Tetromino createTetromino(std::string name) {
 Tetromino tetromino;
 
 // game textures
+std::vector<SDL_Texture*> blockTextures;
 SDL_Texture* blockTexture = nullptr;
-
+SDL_Texture* blockRedTexture = nullptr;
+SDL_Texture* blockGreenTexture = nullptr;
+SDL_Texture* blockYellowTexture = nullptr;
+SDL_Texture* blockBlueTexture = nullptr;
+SDL_Texture* blockLightBlueTexture = nullptr;
+SDL_Texture* blockPinkTexture = nullptr;
+SDL_Texture* blockBlackTexture = nullptr;
 // game rects
 SDL_Rect blockRect;
 
@@ -168,10 +189,51 @@ SDL_Texture* loadTexture(const std::string &path) {
 }
 
 bool loadMedia() {
-	blockTexture = loadTexture("assets/block.png");
-	if(!blockTexture) {
+	blockRedTexture = loadTexture("assets/block-red.png");
+	if(!blockRedTexture) {
 		return false;
 	}
+
+  blockGreenTexture = loadTexture("assets/block-green.png");
+	if(!blockGreenTexture) {
+		return false;
+	}
+
+  blockYellowTexture = loadTexture("assets/block-yellow.jpg");
+	if(!blockYellowTexture) {
+		return false;
+	}
+
+  blockBlueTexture = loadTexture("assets/block-blue.jpg");
+  if(!blockBlueTexture) {
+    return false;
+  }
+
+  blockLightBlueTexture = loadTexture("assets/block-light-blue.jpg");
+  if(!blockLightBlueTexture) {
+    return false;
+  }
+
+  blockPinkTexture = loadTexture("assets/block-pink.jpg");
+  if(!blockPinkTexture) {
+    return false;
+  }
+
+  blockBlackTexture = loadTexture("assets/block-black.jpg");
+  if(!blockBlackTexture) {
+    return false;
+  }
+
+
+  blockTextures.push_back(nullptr);
+  blockTextures.push_back(blockRedTexture);
+  blockTextures.push_back(blockGreenTexture);
+  blockTextures.push_back(blockYellowTexture);
+  blockTextures.push_back(blockBlueTexture);
+  blockTextures.push_back(blockLightBlueTexture);
+  blockTextures.push_back(blockPinkTexture);
+  blockTextures.push_back(blockBlackTexture);
+
   return true;
 }
 
@@ -203,9 +265,9 @@ void drawPlay() {
   // draw landed tetrominos
 	for (auto row = 0; row < tiles.size(); row++) {
 		for (auto col = 0; col < tiles[row].size(); col++) {
-			if (tiles[row][col] == 1) {
+			if (tiles[row][col] != 0) {
 				blockRect = { BLOCK_WIDTH * col, BLOCK_HEIGHT * row, BLOCK_WIDTH, BLOCK_HEIGHT };
-				SDL_RenderCopy(renderer, blockTexture, nullptr, &blockRect);
+				SDL_RenderCopy(renderer, blockTextures[tiles[row][col]], nullptr, &blockRect);
 			}
 		}
 	}
@@ -217,14 +279,14 @@ void drawTetromino() {
   // draws a falling tetromino
   for (auto row = 0; row < tetromino.shape.size(); row++) {
     for (auto col = 0; col < tetromino.shape[row].size(); col++) {
-      if (tetromino.shape[row][col] == 1) {
+      if (tetromino.shape[row][col] != 0) {
         SDL_Rect rect = {
           tetromino.x + BLOCK_WIDTH * col,
           tetromino.y + BLOCK_HEIGHT * row,
           BLOCK_WIDTH,
           BLOCK_HEIGHT
         };
-        SDL_RenderCopy(renderer, blockTexture, nullptr, &rect);
+        SDL_RenderCopy(renderer, blockTextures[tetromino.shape[row][col]], nullptr, &rect);
       }
     }
   }
@@ -249,13 +311,13 @@ void generateTetromino() {
 bool canMoveLeft() {
   for (auto row = 0; row < tetromino.shape.size(); row++) {
     for (auto col = 0; col < tetromino.shape[row].size(); col++) {
-      if (tetromino.shape[row][col] == 1) {
+      if (tetromino.shape[row][col] != 0) {
         // check left board collision
         if (tetromino.col - 1 + col < 0) {
           return false;
         }
         // check left collsion with tetros
-        if (tiles[tetromino.row + row][tetromino.col - 1 + col] == 1) {
+        if (tiles[tetromino.row + row][tetromino.col - 1 + col] != 0) {
           return false;
         }
       }
@@ -267,13 +329,13 @@ bool canMoveLeft() {
 bool canMoveRight() {
   for (auto row = 0; row < tetromino.shape.size(); row++) {
     for (auto col = 0; col < tetromino.shape[row].size(); col++) {
-      if (tetromino.shape[row][col] == 1) {
+      if (tetromino.shape[row][col] != 0) {
         // check bottom board collision
         if (tetromino.col + 1 + col > 9) {
           return false;
         }
         // check right collision with tetros
-        if (tiles[tetromino.row + row][tetromino.col + 1 + col] == 1) {
+        if (tiles[tetromino.row + row][tetromino.col + 1 + col] != 0) {
           return false;
         }
       }
@@ -285,13 +347,13 @@ bool canMoveRight() {
 bool canMoveDown() {
   for (auto row = 0; row < tetromino.shape.size(); row++) {
     for (auto col = 0; col < tetromino.shape[row].size(); col++) {
-      if (tetromino.shape[row][col] == 1) {
+      if (tetromino.shape[row][col] != 0) {
         // check bottom board collision
         if (tetromino.row + 1 + row > 15) {
           return false;
         }
         // check bottom collision with tetros
-        if (tiles[tetromino.row + 1 + row][tetromino.col + col] == 1) {
+        if (tiles[tetromino.row + 1 + row][tetromino.col + col] != 0) {
           return false;
         }
       }
@@ -302,7 +364,7 @@ bool canMoveDown() {
 
 bool isRowComplete(const std::array<int, 10> row) {
   for (auto col = 0; col < row.size(); col++) {
-    if (row[col] != 1) {
+    if (row[col] == 0) {
       return false;
     }
   }
@@ -353,10 +415,10 @@ void updateTetromino() {
       tetromino.isMoving = false;
     }
     if (tetromino.targetX < tetromino.x) {
-      tetromino.x -= 8;
+      tetromino.x -= 16;
     }
     if (tetromino.targetX > tetromino.x) {
-      tetromino.x += 8;
+      tetromino.x += 16;
     }
   }
 
@@ -368,8 +430,8 @@ void updateTetromino() {
     else {
       for (auto row = 0; row < tetromino.shape.size(); row++) {
         for (auto col = 0; col < tetromino.shape[row].size(); col++) {
-          if (tetromino.shape[row][col] == 1) {
-            tiles[tetromino.row + row][tetromino.col + col] = 1;
+          if (tetromino.shape[row][col] != 0) {
+            tiles[tetromino.row + row][tetromino.col + col] = tetromino.shape[row][col];
           }
         }
       }
@@ -385,13 +447,11 @@ void updateTetromino() {
   for (auto rowIdx = 0; rowIdx < tiles.size(); rowIdx++) {
     if (isRowComplete(tiles[rowIdx])) {
       removeRow(rowIdx);
-      SDL_Delay(250);
+      SDL_Delay(500);
       // move all rows above the deleted one down by one col
-      for (auto rowAboveIdx = 0; rowAboveIdx < rowIdx; rowAboveIdx++) {
+      for (auto rowAboveIdx = rowIdx -1; rowAboveIdx >= 0; rowAboveIdx--) {
         for (auto col = 0; col < tiles[rowAboveIdx].size(); col++) {
-          if (tiles[rowAboveIdx][col] == 1) {
-            tiles[rowAboveIdx + 1][col] = 1;
-          }
+          tiles[rowAboveIdx + 1][col] = tiles[rowAboveIdx][col];
         }
       }
     }
