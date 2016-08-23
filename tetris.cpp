8cd0455e051aaaaf5ef3e5ba102aed3daa954405
theadedmonkey <theadedmonkey@gmail.com>
2016-08-23 17:27:11 +0200

    added score

--- tetris.cpp
+++ tetris.cpp
@@ -22,16 +22,26 @@ SDL_Renderer* renderer = nullptr;
 
 // 22 rows 10 cols
 std::array<std::array<int, 10>, 22> tiles;
-// the score for clearing lines depends on the level
+// the score depends both on the number of lines cleared at once
+// and the level the game is.
 int score = 0;
+
 // advance one level when clear 10 lines
 int level = 0;
-// the number of lines cleared
+// the number of lines cleared in a single drop
+// minimun is 1 maximun is 4;
 int clearedLinesCount = 0;
+// the number of lines cleared until next level
+// to reach a next level must clear 10 lines.
+int clearedLinesToNextLevelCount = 0;
 
-TTF_Font* font = nullptr;
-const int FONT_SIZE = 64;
-SDL_Color FONT_COLOR = { 255, 255, 000, 255 };
+TTF_Font* levelFont = nullptr;
+const int LEVEL_FONT_SIZE = 64;
+SDL_Color LEVEL_FONT_COLOR = { 255, 255, 000, 255 };
+
+TTF_Font* scoreFont = nullptr;
+const int SCORE_FONT_SIZE = 18;
+SDL_Color SCORE_FONT_COLOR = { 255, 255, 255, 255 };
 
 int tickDurationDefault = 500.0f;
 int tickDuration = tickDurationDefault;
@@ -51,6 +61,7 @@ const int BOARD_BACKGROUND_HEIGHT_HALF = BOARD_BACKGROUND_HEIGHT / 2;
 
 // board background position
 const int BOARD_BACKGROUND_LEFT = SCREEN_WIDTH_HALF - BOARD_BACKGROUND_WIDTH_HALF;
+const int BOARD_BACKGROUND_RIGHT = SCREEN_WIDTH_HALF + BOARD_BACKGROUND_WIDTH_HALF;
 const int BOARD_BACKGROUND_TOP = SCREEN_HEIGHT_HALF - BOARD_BACKGROUND_HEIGHT_HALF;
 
 // tetrominos
@@ -224,6 +235,7 @@ Tetromino createTetromino(std::string name) {
 Tetromino tetromino;
 
 SDL_Texture* levelTextTexture = nullptr;
+SDL_Texture* scoreTextTexture = nullptr;
 SDL_Texture* playBackgroundTexture = nullptr;
 // mino textures
 std::vector<SDL_Texture*> blockTextures;
@@ -247,6 +259,8 @@ SDL_Rect boardBackgroundRect = {
 };
 SDL_Rect levelTextRect;
 SDL_Rect levelBackgroundRect = { BOARD_BACKGROUND_LEFT - 128 - 32, BOARD_BACKGROUND_TOP, 128, 128 };
+SDL_Rect scoreTextRect;
+SDL_Rect scoreBackgroundRect = { BOARD_BACKGROUND_RIGHT + 32, BOARD_BACKGROUND_TOP, 128, 128 };
 
 bool initSDL();
 SDL_Texture* loadTexture(const std::string& path);
@@ -254,11 +268,13 @@ SDL_Texture* loadTextTexture(std::string text, SDL_Color color);
 bool loadMedia();
 bool initGame();
 void resetPlay();
+int calcScore(int level, int lines);
 void drawPlay();
 void drawPlayBackground();
 void drawBoardBackground();
 void drawTetromino();
 void drawLevel();
+void drawScore();
 int random(int min, int max);
 void generateTetromino();
 bool canRotate(int rotationIdx);
@@ -329,8 +345,13 @@ SDL_Texture* loadTextTexture(TTF_Font* font, std::string text, SDL_Color color) 
 }
 
 bool loadMedia() {
-  font = TTF_OpenFont("assets/Exo2-Regular.ttf", FONT_SIZE);
-  if(!font) {
+  levelFont = TTF_OpenFont("assets/Exo2-Regular.ttf", LEVEL_FONT_SIZE);
+  if(!levelFont) {
+    return false;
+  }
+
+  scoreFont = TTF_OpenFont("assets/Exo2-Regular.ttf", SCORE_FONT_SIZE);
+  if(!scoreFont) {
     return false;
   }
 
@@ -407,6 +428,32 @@ void resetPlay() {
   generateTetromino();
 }
 
+/*
+ * TODO:
+ * For each piece, the game also awards the number of points equal to the number
+ * of grid spaces that the player has continuously soft dropped the piece.
+ * Unlike the points for lines, this does not increase per level.
+ */
+int calcScore(int level, int linesCount) {
+  int lineBonus = 0;
+  switch(linesCount) {
+    case 1 :
+      lineBonus = 40;
+      break;
+    case 2 :
+      lineBonus = 100;
+      break;
+    case 3 :
+      lineBonus = 300;
+      break;
+    case 4 :
+      lineBonus = 1200;
+      break;
+  };
+
+  return lineBonus * (level + 1);
+}
+
 void drawPlay() {
 	SDL_RenderClear(renderer);
 
@@ -414,6 +461,7 @@ void drawPlay() {
   drawBoardBackground();
   drawTetromino();
   drawLevel();
+  drawScore();
 
   // draw landed tetrominos
 	for (auto row = 0; row < tiles.size(); row++) {
@@ -472,7 +520,7 @@ void drawLevel() {
 		SDL_DestroyTexture(levelTextTexture);
 	}
 
-	levelTextTexture = loadTextTexture(font, std::to_string(level), FONT_COLOR);
+	levelTextTexture = loadTextTexture(levelFont, std::to_string(level), LEVEL_FONT_COLOR);
 	int levelTextRectW, levelTextRectH;
 	SDL_QueryTexture(levelTextTexture, nullptr, nullptr, &levelTextRectW, &levelTextRectH);
 
@@ -483,6 +531,29 @@ void drawLevel() {
     levelTextRectH
   };
 	SDL_RenderCopy(renderer, levelTextTexture, nullptr, &levelTextRect);
+}
+
+void drawScore() {
+  // render score background
+  SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+  SDL_RenderFillRect(renderer, &scoreBackgroundRect);
+
+  // render score text;
+  if(scoreTextTexture) {
+		SDL_DestroyTexture(scoreTextTexture);
+	}
+
+	scoreTextTexture = loadTextTexture(scoreFont, std::to_string(score), SCORE_FONT_COLOR);
+	int scoreTextRectW, scoreTextRectH;
+	SDL_QueryTexture(scoreTextTexture, nullptr, nullptr, &scoreTextRectW, &scoreTextRectH);
+
+	scoreTextRect = {
+    scoreBackgroundRect.x + scoreBackgroundRect.w / 2 - scoreTextRectW / 2,
+    scoreBackgroundRect.y + scoreBackgroundRect.h / 2 - scoreTextRectH / 2,
+    scoreTextRectW,
+    scoreTextRectH
+  };
+	SDL_RenderCopy(renderer, scoreTextTexture, nullptr, &scoreTextRect);
 }
 
 // range : [min, max) max no inclusive
@@ -608,8 +679,6 @@ bool hasLanded = false;
 
 void updateTetromino() {
 
-  // std::cout << tetromino.row << " : " << tetromino.col << std::endl;
-
   tickCurrent = SDL_GetTicks();
   tickAccum += tickCurrent - tickLast;
   if (tickAccum > tickDuration) {
@@ -632,17 +701,20 @@ void updateTetromino() {
       for (auto rowIdx = 0; rowIdx < tiles.size(); rowIdx++) {
         if (isRowComplete(tiles[rowIdx])) {
           removeRow(rowIdx);
-          clearedLinesCount += 1;
-          if (clearedLinesCount == 2) {
-            clearedLinesCount = 0;
+          clearedLinesToNextLevelCount += 1;
+          if (clearedLinesToNextLevelCount == 2) {
+            clearedLinesToNextLevelCount = 0;
             level += 1;
           }
+
+          clearedLinesCount += 1;
           // move all rows above the deleted one down by one col
           for (auto rowAboveIdx = rowIdx -1; rowAboveIdx >= 0; rowAboveIdx--) {
             for (auto col = 0; col < tiles[rowAboveIdx].size(); col++) {
               tiles[rowAboveIdx + 1][col] = tiles[rowAboveIdx][col];
             }
           }
+          score += calcScore(level, clearedLinesCount);
           SDL_Delay(500);
         }
       }
@@ -705,7 +777,6 @@ int main( int argc, char* args[] ) {
         }
         if(event.key.keysym.sym == SDLK_DOWN) {
           tickDuration = hasLanded ? tickDurationDefault : tickDuration / 2;
-          // tickDuration /= 2;
         }
       }
     }
